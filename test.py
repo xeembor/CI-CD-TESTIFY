d082b6f166904ff464801f2ca11e34c85d42fb26
xeembor <91500279+xeembor@users.noreply.github.com>
2023-08-30 02:54:45 +0100

    format

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import unittest
 
 
 class TestMain(unittest.TestCase):
-
+    
     def test_addition(self):
         self.assertEqual(main.addition(1, 2), 3, "Should be 3")
         self.assertEqual(main.addition(5, 5), 10, "Should be 10")
